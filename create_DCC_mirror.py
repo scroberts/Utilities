100ec316182862de7493bcc05c22468d6a42862a
Paul Barnes <barnesp@hawaii.edu>
2016-06-15 08:17:45 -1000

    Initial Commit
    This is the initial commit for the script that allows a user to create
    a mirror of a DCC collection on their local computer.

---
+++ create_DCC_mirror.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# external modules
+
+# my modules
+import DCC
+import Config as CF
+import FileSys
+
+# Prerequisite:
+#	1. Have a collection number for which collections and documents you would like mirrored on 
+#	your personal computer. If there are collections you would like to exclude ensure
+#	that they are appended to the 'exclude' variable with a comma
+#	2. Have a directory in which you would like to save all the documents on your PC
+
+# Expected Output:
+#	As this script runs all collections and documents (except for excluded ones)
+#	will be printed to the screen. Once traversed, it will the display all
+#	information about documents located in the collection chosen and determine if they already exist
+#	in the directory selected. Once the process is complete
+#	you will be able to locate all files in the directory selected.
+
+
+# Login to DCC
+s = DCC.login(CF.dcc_url + CF.dcc_login)
+
+coll = 'Collection-8277'
+dir = '/Users/paulb/Documents/TMT/Python/DCC Files/Configuration Controls/'
+exclude = ['Collection-10582']
+
+FileSys.create_DCC_mirror(s, coll, dir, SaveFiles = True, MaxFileSize = 10000000, Exclude = exclude)
